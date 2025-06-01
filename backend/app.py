4b884e1ea2380001c2db310fca226823f4cd4d2b
amanagarwal <amangrwl30@gmail.com>
2025-06-01 12:43:34 +0530

    Some Demo Python code added

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from yellow import router as yellow_router
 from audio import router as audio_router
 from call import router as call_router
-from mydemo import app as mydemo_app  # Import the FastAPI app from mydemo
-from capture import router as capture_router  # Import the router from capture
-from pagedemo import router as pagedemo_router  # Import the router from pagedemo
+#from mydemo import app as mydemo_app  # Import the FastAPI app from mydemo
+#from capture import router as capture_router  # Import the router from capture
+#from pagedemo import router as pagedemo_router  # Import the router from pagedemo
 
 app = FastAPI()
 
@@ -22,6 +22,6 @@ app.include_router(yellow_router, prefix="/yellow", tags=["yellow"])
 #app.include_router(shopify_router, prefix="/shopify", tags=["shopify"])
 app.include_router(audio_router, prefix="/audio", tags=["audio"])
 app.include_router(call_router, prefix="/call", tags=["call"])
-app.include_router(mydemo_app.router, prefix="/leads", tags=["leads"])  # Include mydemo routes under /leads prefix
-app.include_router(capture_router, prefix="/capture", tags=["capture"])  # Add capture routes
-app.include_router(pagedemo_router, prefix="/page", tags=["page"])  # Add pagedemo routes
+#app.include_router(mydemo_app.router, prefix="/leads", tags=["leads"])  # Include mydemo routes under /leads prefix
+#app.include_router(capture_router, prefix="/capture", tags=["capture"])  # Add capture routes
+#app.include_router(pagedemo_router, prefix="/page", tags=["page"])  # Add pagedemo routes
